2762dc0d2d8c4601fa8be68384099f0d14cff290
pagiron <pagiron@scl.edu.gt>
2024-05-28 09:28:37 -0600

    Prueba corta

---
+++ header/header.js
@@ -0,0 +1,10 @@
+let div_footer=document.querySelector(".footer")
+
+div_footer.innerHTML = `
+<div class="header1">
+    <li>Bloque 1</li>
+    <li>Bloque 2</li>
+    <li>Bloque 3</li>
+    <li>Bloque 4</li>
+    <li>Bloque 5</li>
+</div>`
